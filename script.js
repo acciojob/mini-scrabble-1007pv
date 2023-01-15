c7d40af7e5c1e331cbd44eae8fd12a1af3f451a4
Pankaj Verma <84775453+1007pv@users.noreply.github.com>
2023-01-15 19:36:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-//your code here
+function wordCount() {
+ var text = document.getElementById("evaluatedText").value;
+ var count = 0;
+ var split = text.split('');
+ for (var i = 0; i < split.length; i++) {
+  if (split[i] != "") {
+   count ++;
+  }
+ }
+ document.getElementById("letterCount").innerHTML = count;
+}
